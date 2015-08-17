72cde601c1e3fe2b4c10f244272ee338a17b092c
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-17 15:54:22 -0400

    better newline handling

--- index.js
+++ index.js
@@ -14,60 +14,53 @@ function inject(str, snippet, opts) {
   }
 
   opts = opts || {};
-  var delims = opts.delims || ['<!--', '-->'];
+  var delims = opts.delimiters || ['<!--', '-->'];
   var name = opts.marker || 'snippet';
   var regex = opts.regex || toRegex(name, delims);
-  var open = opts.open || openDelim(name, delims);
-  var close = opts.close || closeDelim(name, delims);
-  var strip = stripMarkers(name, delims);
+  var open = openDelim(name, delims, opts);
+  var close = closeDelim(name, delims, opts);
+  var trailing = /(\n+)$/.exec(str);
 
   // get any existing sections
-  var sections = split(str, regex);
-  var contents = toSnippet(snippet, strip);
+  var sections = split(str, regex, opts);
+  var contents = stripTags(snippet, regex, opts);
 
   // no snippet markers, so just append the string
   if (sections.length === 1 && opts.append !== false) {
     return str + contents;
   }
 
-  var a = sections.shift();
-  var b = sections.pop();
+  var start = emit(sections.shift(), opts);
+  var end = emit(sections.pop(), opts);
   var len = sections.length;
+
   var inner;
-
   if (len > 1 && opts.multiple !== false) {
-    inner = sections.join(contents);
+    inner = sections.join(emit(contents, opts));
   } else if (len > 1) {
-    inner = sections.shift();
-    inner += contents;
-    inner += sections.join('\n');
-  }
+    inner = emit(sections.shift(), opts);
+    inner += emit(contents, opts);
+    inner += emit(sections.join('\n'), opts);
 
-  if (len === 1) {
+  } else if (len === 1) {
     inner = update(sections[0], contents, opts);
+
   } else if (!len) {
-    inner = contents;
+    inner = emit(contents, opts);
   }
 
   var middle = open + inner + close;
-  if (opts.stripPlaceholders === true) {
+  if (opts.stripTags === true) {
     middle = inner;
   }
 
-  return a + middle + b;
+  var res = start + middle + end;
+  return trimRight(res) + (trailing ? trailing[0] : '');
 }
 
 /**
  * Snippet utils
  */
-
-function emit(str, fn) {
-  return toString(str).trim() + '\n';
-}
-
-function toSnippet(str, fn) {
-  return emit(fn(str));
-}
 
 function update(str, snippet, opts) {
   switch(opts.action) {
@@ -92,35 +85,56 @@ function toString(str) {
   return str == null ? '' : str;
 }
 
-function split(str, re) {
-  return toString(str).split(re).map(emit);
+function trimRight(str) {
+  return str.replace(/\s+$/, '');
+}
+
+function split(str, re, opts) {
+  return toString(str).split(re).map(function (seg) {
+    return emit(seg, opts);
+  });
+}
+
+function emit(str, opts) {
+  return opts.newlines
+    ? toString(str).trim() + '\n'
+    : toString(str);
 }
 
 /**
  * Delimiter-related utils
  */
 
+function memoize(key, val) {
+  if (cache.hasOwnProperty(key)) {
+    return cache[key];
+  }
+  return (cache[key] = val);
+}
+
 function toRegex(name, delims) {
-  return cache[name] || (cache[name] = new RegExp(toMarker(name, delims), 'g'));
+  var key = name + delims.join('_');
+  return memoize(key, new RegExp(toMarker(name, delims), 'g'));
+}
+
+function escape(str) {
+  return str.replace(/(\W)/g, '\\$1');
 }
 
 function toMarker(name, delims) {
-  return '(?:' + delims[0] + '\\s*(?:end)?' + name + '\\s*' + delims[1] + ')';
+  return '(?:' + escape(delims[0]) + '\\s*(?:end)?' + name + '\\s*' + escape(delims[1]) + ')';
 }
 
-function stripMarkers(name, delims) {
-  var re = toRegex(name, delims);
-  return function(str) {
-    return emit(split(str, re).join(''));
-  };
+function stripTags(str, re, opts) {
+  return emit(split(str, re, opts).join(''), opts);
 }
 
-function openDelim(name, delims) {
-  return emit(delims[0] + ' ' + name + ' ' + delims[1]);
+function openDelim(name, delims, opts) {
+  return emit(delims[0] + ' ' + name + ' ' + delims[1], opts);
 }
 
-function closeDelim(name, delims) {
-  return emit(delims[0] + ' end' + name + ' ' + delims[1]);
+function closeDelim(name, delims, opts) {
+  return emit(delims[0] + ' end' + name + ' ' + delims[1], opts);
 }
 
 /**
@@ -128,3 +142,15 @@ function closeDelim(name, delims) {
  */
 
 module.exports = inject;
+
+/**
+ * Expose `stripTags`
+ */
+
+module.exports.stripTags = function (str, opts) {
+  opts = opts || {};
+  var delims = opts.delimiters || ['<!--', '-->'];
+  var name = opts.marker || 'snippet';
+  var regex = opts.regex || toRegex(name, delims);
+  return stripTags(str, regex, opts);
+};
--- test.js
+++ test.js
@@ -4,21 +4,51 @@
 var should = require('should');
 var inject = require('./');
 
-describe('inject', function () {
-  it('should inject a snippet into a string with placeholders:', function () {
-    var str = 'before <!-- snippet --> after';
-    var actual = inject(str, 'foo');
-    actual.should.equal('before\n<!-- snippet -->\nfoo\n<!-- endsnippet -->\nafter\n');
+describe('inject snippet', function () {
+  describe('errors', function () {
+    it('should throw an error when invalid arguments are passed:', function () {
+      (function () {
+        inject();
+      }).should.throw('expected a string as the first argument.');
+    });
   });
 
-  it('should inject a snippet into a string without placeholders:', function () {
-    var str = 'before <!-- snippet --> after';
-    inject(str, 'foo', {stripPlaceholders: true}).should.equal('before\nfoo\nafter\n');
+  describe('inject', function () {
+    it('should inject a snippet into a string with one placeholder:', function () {
+      var str = 'a <!-- snippet --> b';
+      var actual = inject(str, 'foo');
+      actual.should.equal('a <!-- snippet -->foo<!-- endsnippet --> b');
+    });
+
+    it('should inject a snippet into a string with before/after placeholders:', function () {
+      var str = 'a <!-- snippet -->\nfoo\n<!-- endsnippet --> b';
+      var actual = inject(str, 'bar\n');
+      actual.should.equal('a <!-- snippet -->\nfoo\nbar\n<!-- endsnippet --> b');
+    });
   });
 
-  it('should throw an error when invalid arguments are passed:', function () {
-    (function () {
-      inject();
-    }).should.throw('expected a string as the first argument.');
+  describe('options', function () {
+    it('should inject a snippet into a string without placeholders:', function () {
+      var str = 'a <!-- snippet --> b';
+      inject(str, 'foo', {stripTags: true}).should.equal('a foo b');
+    });
+
+    it('should add normalized newlines around snippets:', function () {
+      var str = 'a <!-- snippet --> b';
+      var actual = inject(str, 'foo', {newlines: true});
+      actual.should.equal('a\n<!-- snippet -->\nfoo\n<!-- endsnippet -->\nb');
+    });
+
+    it('should use custom delimiters:', function () {
+      var str = 'a {{!snippet}} b';
+      var actual = inject(str, 'foo', {delimiters: ['{{!', '}}']});
+      actual.should.equal('a {{! snippet }}foo{{! endsnippet }} b');
+    });
+
+    it('should use custom delimiters:', function () {
+      var str = 'a {{!snippet}} b';
+      var actual = inject(str, 'foo', {delimiters: ['{{!', '}}']});
+      actual.should.equal('a {{! snippet }}foo{{! endsnippet }} b');
+    });
   });
 });
